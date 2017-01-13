8bc11c4ca55871e9024ca322ace9e397d3fac148
Yana Valasatava <yana.valasatava@rcsb.org>
2017-01-12 19:25:35 -0800

    use of VCF parser

---
+++ src/main/java/org/rcsb/genevariation/parser/StreamReader.java
@@ -0,0 +1,36 @@
+package org.rcsb.genevariation.parser;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+ 
+public class StreamReader {
+ 
+    @SuppressWarnings("resource")
+	public static void main(String[] args) {
+ 
+        long startTime = System.nanoTime();
+        
+        String path = "/Users/yana/data/genevariation/vcfExample.vcf";
+        Path file = Paths.get(path);
+        try {
+            //Java 8: Stream class
+            Stream<String> lines = Files.lines( file, StandardCharsets.UTF_8 ); 
+            for( String line : (Iterable<String>) lines::iterator )
+            {
+               System.out.println(line);
+            }
+        
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+ 
+        long endTime = System.nanoTime();
+        long elapsedTimeInMillis = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
+        System.out.println("Total elapsed time: " + elapsedTimeInMillis + " ms");
+    }
+}
--- src/main/java/org/rcsb/genevariation/sandbox/Test.java
+++ src/main/java/org/rcsb/genevariation/sandbox/Test.java
@@ -1,5 +1,26 @@
 package org.rcsb.genevariation.sandbox;
 
-public class Test {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+import org.pharmgkb.parser.vcf.VcfParser;
+
+public class Test {
+	
+	public static void main(String[] args) throws IOException {
+		
+		String path = "/Users/yana/data/genevariation/vcfExample.vcf";
+		Path file = Paths.get(path);
+				
+		VcfParser parser = new VcfParser
+				.Builder()
+				.fromFile(file)
+		        .parseWith((metadata, position, sampleData) -> {
+		        	  System.out.println(position.getChromosome() +" " + position.getPosition()+" "+position.getAltBases().toString());
+		         })
+				.build();
+		parser.parse();
+		
+	}
 }
