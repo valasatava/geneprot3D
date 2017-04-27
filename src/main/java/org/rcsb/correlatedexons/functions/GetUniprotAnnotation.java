b1e9684f94c7d7aaf2c781569e0e2bd9c4c2fbc1
Yana Valasatava <yana.valasatava@rcsb.org>
2017-04-27 14:19:03 -0700

    code refactoring

--- src/main/java/org/rcsb/correlatedexons/functions/GetUniprotAnnotation.java
+++ src/main/java/org/rcsb/correlatedexons/functions/GetUniprotAnnotation.java
@@ -25,29 +25,6 @@ import static org.apache.spark.sql.functions.col;
  */
 public class GetUniprotAnnotation {
 
-    public static void foo() throws FileNotFoundException, JAXBException {
-
-        String uniProtId = "Q8V5E0";
-        Uniprot up = RCSBUniProtMirror.getUniProtFromFile(uniProtId);
-
-        for(Entry e : up.getEntry()){
-            for (FeatureType ft : e.getFeature()){
-
-                // ranged features
-                if ( ft.getLocation() != null && ft.getLocation().getBegin() != null && ft.getLocation().getEnd() != null)
-                    System.out.println("RANGED: " + ft.getDescription()+" --- "+
-                            ft.getType() + " --- " +
-                            ft.getLocation().getBegin().getPosition() + " : " + ft.getLocation().getEnd().getPosition());
-
-                // single resiude position features
-                if ( ft.getLocation() != null && ft.getLocation().getPosition() != null && ft.getLocation().getPosition().getPosition() != null)
-                    System.out.println("FT: " + ft.getDescription()+" --- "+
-                            ft.getType() + " --- " +
-                            ft.getLocation().getPosition().getPosition());
-            }
-        }
-    }
-
     public static  List<String> run(String chr) throws FileNotFoundException, JAXBException {
 
         Dataset<Row> mapping = SaprkUtils.getSparkSession()
