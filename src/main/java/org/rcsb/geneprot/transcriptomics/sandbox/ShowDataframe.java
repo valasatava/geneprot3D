602f30fab91e4a06b6a826b61a7231b45eec87f2
valasatava <yana.valasatava@gmail.com>
2017-06-30 10:46:19 -0700

    Minor updates

--- src/main/java/org/rcsb/geneprot/transcriptomics/sandbox/ShowDataframe.java
+++ src/main/java/org/rcsb/geneprot/transcriptomics/sandbox/ShowDataframe.java
@@ -4,6 +4,8 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.rcsb.geneprot.common.utils.SaprkUtils;
 
+import static org.apache.spark.sql.functions.col;
+
 /**
  * Created by yana on 4/19/17.
  */
@@ -12,8 +14,8 @@ public class ShowDataframe {
     public static void main(String[] args) {
 
         Dataset<Row>  d = SaprkUtils.getSparkSession().read()
-                .parquet("/Users/yana/data/genevariation/parquet/human-homology-models");
-        d.show();
+                .parquet("/Users/yana/spark/parquet/uniprotpdb/20170629");
+        d.filter(col("pdbId").equalTo("5IRC").and(col("chainId").equalTo("F"))).show();
 
         SaprkUtils.stopSparkSession();
     }
