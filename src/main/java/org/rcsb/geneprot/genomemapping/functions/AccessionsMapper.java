2b2864312cfb3f4bb649cec667c835dbaa504a2a
valasatava <yana.valasatava@gmail.com>
2017-10-20 13:38:06 -0700

    adding dependency on mojave

---
+++ src/main/java/org/rcsb/geneprot/genomemapping/functions/AccessionsMapper.java
@@ -0,0 +1,10 @@
+package org.rcsb.geneprot.genomemapping.functions;
+
+/**
+ * Created by Yana Valasatava on 10/5/17.
+ */
+public class AccessionsMapper {
+
+    public static void main(String[] args) {
+    }
+}
